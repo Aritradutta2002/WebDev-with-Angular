a6ef81ff6efb7bd3345f3cfeb2e3f88c9a8a8b81
ARITRA DUTTA <aritradutta049@gmail.com>
2025-01-27 01:04:52 +0530

    first time

--- Workflow Management Angular/src/app/app.component.ts
+++ Workflow Management Angular/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { TaskComponent } from './task/task.component';
-import { DUMMY_USERS } from './dummy-users';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 import { NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
@@ -13,16 +14,14 @@ import { NgFor } from '@angular/common';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-
   users = DUMMY_USERS;
   selectedUserId !: string;
 
   get selectedUser() {
-    return this.users.find((users) => users.id === this.selectedUserId);
+    return this.users.find((users) => users.id === this.selectedUserId)!;
   }
 
-  onSelectUser(id: string) {
+  onSelectedUser(id: string) {
     this.selectedUserId = id;
   }
-
 }
