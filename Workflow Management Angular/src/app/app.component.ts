90589327ceb0aac6629bf48a3a993fb185996522
ARITRA DUTTA <aritradutta049@gmail.com>
2025-02-03 18:39:51 +0530

    all modified final

--- Workflow Management Angular/src/app/app.component.ts
+++ Workflow Management Angular/src/app/app.component.ts
@@ -1,27 +1,20 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { TaskComponent } from './task/task.component';
-import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
-import { NgFor } from '@angular/common';
-
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, TaskComponent, UserComponent, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId !: string;
+  selectedUserId?: string;
 
   get selectedUser() {
-    return this.users.find((users) => users.id === this.selectedUserId)!;
+    return this.users.find((user) => user.id === this.selectedUserId);
   }
 
-  onSelectedUser(id: string) {
+  onSelectUser(id: string) {
     this.selectedUserId = id;
   }
 }
